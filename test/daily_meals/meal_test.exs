9f5a857c656f3b812eb2fb3ffb08735b98ab08d1
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-16 21:30:17 -0300

    created meal struct test

---
+++ test/daily_meals/meal_test.exs
@@ -0,0 +1,63 @@
+defmodule DailyMeals.MealTest do
+  use DailyMeals.DataCase, async: true
+  alias DailyMeals.Meal
+  alias Ecto.Changeset
+
+  describe "changeset/2" do
+    test "when all params are valid, returns a valid changeset" do
+      params = %{
+        description: "comida muito muito gostosa",
+        date: UTCDateTime.from_date(Date.new!(2023, 10, 10)),
+        calories: "1 cal"
+      }
+
+      response = Meal.changeset(params)
+
+      assert %Changeset{changes: %{description: "comida muito muito gostosa"}, valid?: true} =
+               response
+    end
+
+    test "when updating a changeset, returns invalid changeset with the given changes" do
+      params = %{
+        description: "comida muito muito gostosa",
+        date: UTCDateTime.from_date(Date.new!(2023, 10, 10)),
+        calories: "1 cal"
+      }
+
+      new_date = UTCDateTime.from_date(Date.new!(2021, 11, 11))
+
+      update_params = %{
+        description: "comida muito delicia",
+        date: new_date
+      }
+
+      response = params |> Meal.changeset() |> Meal.changeset(update_params)
+
+      assert %Changeset{
+               changes: %{
+                 description: "comida muito delicia",
+                 date: new_date
+               },
+               valid?: true
+             } = response
+    end
+
+    test "when there are some error, returns an invalid changeset" do
+      params = %{
+        description: "comida",
+        date: "2023, 10, 10",
+        calories: "1 c"
+      }
+
+      response = Meal.changeset(params)
+
+      expected_response = %{
+        calories: ["has invalid format"],
+        date: ["is invalid"],
+        description: ["should be at least 10 character(s)"]
+      }
+
+      assert errors_on(response) == expected_response
+    end
+  end
+end
